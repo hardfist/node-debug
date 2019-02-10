0707a1d7fa33abc5fa5fe951d4461cd6d4597ab9
hardfist <1562502418@qq.com>
2019-02-10 13:09:12 +0800

    add node-clinic
    Change-Id: Iaeb906f372ec2f4bbf55ffad99691e76c67dcc22

--- flamegraph/app.js
+++ flamegraph/app.js
@@ -1,24 +1,37 @@
-'use strict'
+"use strict";
 
-const restify = require('restify')
-const server = restify.createServer()
+const restify = require("restify");
+const server = restify.createServer();
 
-function sleep (ms) {
-  const future = Date.now() + ms
+function sleepA(ms) {
+  const future = Date.now() + ms;
   while (Date.now() < future);
 }
+function sleepB(ms) {
+  const future = Date.now() + ms;
+  while (Date.now() < future);
+}
+function a() {
+  for (let i = 0; i < 1e5; i++) {}
+}
+function b() {
+  for (let i = 0; i < 1e5; i++) {}
+}
 
-server.get('/', function (req, res, next) {
-  sleep(30)
-  res.send({})
-  next()
-})
+server.get("/", function(req, res, next) {
+  sleepA(30);
+  a();
+  b();
+  sleepB(40);
+  res.send({});
+  next();
+});
 
 server.listen(3000, () => {
-  console.log('listen at: http://127.0.0.1:3000/')
-})
+  console.log("listen at: http://127.0.0.1:3000/");
+});
 
-process.on('SIGINT', function () {
-  console.error('Caught SIGINT, shutting down.')
-  server.close()
-})
+process.on("SIGINT", function() {
+  console.error("Caught SIGINT, shutting down.");
+  server.close();
+});
